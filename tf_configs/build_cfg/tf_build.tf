db7e1a6a3db2c94957bcd5fbedfd77672cb82433
el <el@el.com>
2023-03-24 15:54:32 +0300

    fix

--- tf_configs/build_cfg/tf_build.tf
+++ tf_configs/build_cfg/tf_build.tf
@@ -56,7 +56,7 @@ resource "yandex_compute_instance" "vm-1" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update && sudo apt install python3 -y",
+      "sudo apt update && sudo apt install python3 python3-pip -y",
       "sudo pip install docker"
     ]
   }
